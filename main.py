df95746b4f8a60422d93ea824ed289baaf169d53
jilsvaghasiya <jilsvaghasiya333@gmail.com>
2022-07-17 21:21:58 +0530

    jinja2 inheritance

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route("/")
 def hello():
     return render_template('index.html')
